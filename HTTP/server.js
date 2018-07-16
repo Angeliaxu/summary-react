e93785182d38b932863f124a4f9f0cde4e298b8e
xuchang <2656995425@qq.com>
2018-07-16 18:20:07 +0800

    cache-control学习

--- HTTP/server.js
+++ HTTP/server.js
@@ -4,11 +4,23 @@ const fs = require('fs');
 http.createServer((req,res)=>{
     console.log('request come',req.url);
 
-    const html = fs.readFileSync('test.html','utf-8');
-    res.writeHead(200,{
-        'Content-Type':'text/html'
-    })
-    res.end(html);
+    
+    if(req.url=='/'){
+        const html = fs.readFileSync('test.html','utf-8');
+        res.writeHead(200,{
+            'Content-Type':'text/html'
+        })
+        res.end(html);
+    }
+    if(req.url == '/script1.js'){
+        
+        res.writeHead(200,{
+            'Content-Type':'text/javascript',
+            'Cache-Control':'max-age=1800,public'
+        })
+        res.end('console.log("script loaded twice")');
+    }
+    
 }).listen(8888);
 
 console.log('server listening on 8888');
