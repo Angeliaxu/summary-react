8b0ffaf3ea2329023f99457485b66de67945b225
xuchang <2656995425@qq.com>
2018-08-07 14:52:22 +0800

    初始vuex

---
+++ VUE/todolist/src/components/count.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+        <div>{{count}}</div>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <Num/>
+    </div>
+</template>
+<script>
+    import Vuex from 'vuex'
+    import Vue from 'vue'
+    import Num from './num.vue'
+
+    export default{
+        name:'count',
+        data(){
+            return{
+                // count:store.state.count
+            }
+        },
+        computed:{
+            count(){
+                return this.store.state.count
+            }
+        },
+        methods:{
+            increment(){
+                this.store.commit('increment')
+            },
+            decrement(){
+                this.store.commit('decrement')
+            }
+        },
+        components:{
+            Num,
+        }
+        
+    }
+</script>
+<style>
+    #app{
+        width: 100px;
+        height:100px;
+        margin:100px auto 0;
+        text-align: center;
+    }
+    #app button{
+        display: inline-block;
+    }
+</style>
--- Vue/todolist/src/main.js
+++ Vue/todolist/src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
-import Todo from './App.vue'
+import Vuex from 'vuex'
+
+import 'es6-promise/auto';
+
+// import Todo from './App.vue'
+
+import Count from './components/count.vue'
 
 Vue.config.productionTip = false
 
+// 在最开头的时候注入vuex
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state:{
+        count:0
+    },
+    mutations:{
+        increment(state){
+            state.count++
+        },
+        decrement(state){
+            state.count--
+        }
+    }
+})
+
 new Vue({
-  render: h => h(Todo)
+  render: h => h(Count),
+  store
 }).$mount('#app')
