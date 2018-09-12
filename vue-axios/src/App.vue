3624936cbd38517242d9cfba51f321d4b6b9e8c6
Angeliaxu <Angeliaxuchang@163.com>
2018-09-12 19:25:59 +0800

    axios

---
+++ vue-axios/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>study axios</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import './libs/axios.js'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  #app{
+    text-align: center;
+    padding: 50px 0;
+  }
+</style>
