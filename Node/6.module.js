4818bc8ce4355fef239613af9a0a7cbac56f7d86
Angeliaxu <2656995425@qq.com>
2018-09-20 23:02:41 +0800

    模块对象

--- Node/6.module.js
+++ Node/6.module.js
@@ -1,6 +1,10 @@
 
 /* 
     exports：
-
+        每一个模块文件中有一个exports对象
+        在模块对象module下有个exports属性
+        虽然exports == module.exports是同一个东西，但是
+        使用上是有一定注意事项的
 */
 console.log(module)
+console.log(exports == module.exports); // true
